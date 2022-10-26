8d37895b7731d1524bfe04a7250a64b733ecfa03
vitarahmada <vita.20003@mhs.unesa.ac.id>
2022-10-26 14:30:44 +0700

    3.1

---
+++ lib/menuutama.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuUtama extends StatefulWidget {
+  const MenuUtama({super.key});
+
+  @override
+  State<MenuUtama> createState() => _MenuUtamaState();
+}
+
+class _MenuUtamaState extends State<MenuUtama> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
